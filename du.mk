d472fecae9f964dbae107f3ca54df3ca1b754451
sub77 <sub77@ymail.com>
2016-11-01 19:20:20 +0100

    falcon: DU-ify

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := du_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
